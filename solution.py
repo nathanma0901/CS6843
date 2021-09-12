ca697b9fff663ef0da2e2380e9b827bbafb66710
nathanma0901 <90540668+nathanma0901@users.noreply.github.com>
2021-09-12 01:35:14 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@
 
 def welcome_assignment_answers(question, answer=None):
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-       answer = "mTLS"
+       answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -18,9 +18,9 @@ def welcome_assignment_answers(question, answer=None):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
        answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-       answer = "2"
+       answer = "1"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-       answer = "2"
+       answer = "1"
     return (answer)
 
 
