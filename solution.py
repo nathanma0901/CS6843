1566fb4d0c1d5395b185cfc8610ac7664e0c2543
nathanma0901 <90540668+nathanma0901@users.noreply.github.com>
2021-12-06 16:03:55 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,127 +1,54 @@
+# import socket module
 from socket import *
-import os
+# In order to terminate the program
 import sys
-import struct
-import time
-import select
-import binascii
-# Should use stdev
 
-ICMP_ECHO_REQUEST = 8
+def webServer(port=13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a server socket
+    serverSocket.bind(("", port))
+    # Fill in start
+    serverSocket.listen(1)
+    # Fill in end
+    while True:
+        # Establish the connection
+        # print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()  # Fill in start      #Fill in end
+        try:
+                message = connectionSocket.recv(1024) # Fill in start    #Fill in end
 
+                filename = message.split()[1]
 
-def checksum(string):
-   csum = 0
-   countTo = (len(string) // 2) * 2
-   count = 0
+                f = open(filename[1:])
 
-   while count < countTo:
-       thisVal = (string[count + 1]) * 256 + (string[count])
-       csum += thisVal
-       csum &= 0xffffffff
-       count += 2
+                outputdata = f.read() # Fill in start     #Fill in end
+                # Send one HTTP header line into socket.
+                # Fill in start
+                connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
 
-   if countTo < len(string):
-       csum += (string[len(string) - 1])
-       csum &= 0xffffffff
+                #connectionSocket.send(outputdata)
+                # Fill in end
+                # Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.close()
+        except IOError:
+        # Send response message for file not found (404)
+        # Fill in start
+            connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n')
+            connectionSocket.send('("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n')
+        # Fill in end
 
-   csum = (csum >> 16) + (csum & 0xffff)
-   csum = csum + (csum >> 16)
-   answer = ~csum
-   answer = answer & 0xffff
-   answer = answer >> 8 | (answer << 8 & 0xff00)
-   return answer
+        # Close client socket
+        # Fill in start
+            connectionSocket.close()
+        # Fill in end
+        #except (ConnectionResetError, BrokenPipeError):
+        #pass
+    serverSocket.close()
+sys.exit()  # Terminate the program after sending the corresponding data
 
+if __name__ == "__main__":
+    webServer(13331)
 
-
-def receiveOnePing(mySocket, ID, timeout, destAddr):
-   timeLeft = timeout
-
-   while 1:
-       startedSelect = time.time()
-       whatReady = select.select([mySocket], [], [], timeLeft)
-       howLongInSelect = (time.time() - startedSelect)
-       if whatReady[0] == []:  # Timeout
-           return "Request timed out."
-
-       timeReceived = time.time()
-       recPacket, addr = mySocket.recvfrom(1024)
-
-       # Fill in start
-       icmpHeader = recPacket[20:28]
-
-       type, code, checksum, id, sequence = struct.unpack("bbHHn", icmpHeader)
-
-       if type == 0 and id == ID:
-           doubleBytes = struct.calcsize("d")
-           sendingTime = struct.unpack("d", recPacket[28:28 + doubleBytes])[0]
-           return (timeReceived-sendingTime)
-
-       # Fetch the ICMP header from the IP packet
-
-       # Fill in end
-       timeLeft = timeLeft - howLongInSelect
-       if timeLeft <= 0:
-           return "Request timed out."
-
-
-def sendOnePing(mySocket, destAddr, ID):
-   # Header is type (8), code (8), checksum (16), id (16), sequence (16)
-
-   myChecksum = 0
-   # Make a dummy header with a 0 checksum
-   # struct -- Interpret strings as packed binary data
-   header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
-   data = struct.pack("d", time.time())
-   # Calculate the checksum on the data and the dummy header.
-   myChecksum = checksum(header + data)
-
-   # Get the right checksum, and put in the header
-
-   if sys.platform == 'darwin':
-       # Convert 16-bit integers from host to network  byte order
-       myChecksum = htons(myChecksum) & 0xffff
-   else:
-       myChecksum = htons(myChecksum)
-
-
-   header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
-   packet = header + data
-
-   mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
-
-
-   # Both LISTS and TUPLES consist of a number of objects
-   # which can be referenced by their position number within the object.
-
-def doOnePing(destAddr, timeout):
-   icmp = getprotobyname("icmp")
-
-
-   # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
-   mySocket = socket(AF_INET, SOCK_RAW, icmp)
-
-   myID = os.getpid() & 0xFFFF  # Return the current process i
-   sendOnePing(mySocket, destAddr, myID)
-   delay = receiveOnePing(mySocket, myID, timeout, destAddr)
-   mySocket.close()
-   return delay
-
-
-def ping(host, timeout=1):
-   # timeout=1 means: If one second goes by without a reply from the server,      # the client assumes that either the client's ping or the server's pong is lost
-   dest = gethostbyname(host)
-   print("Pinging " + dest + " using Python:")
-   print("")
-   # Calculate vars values and return them
-   #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
-   # Send ping requests to a server separated by approximately one second
-   for i in range(0,4):
-       delay = doOnePing(dest, timeout)
-       print(delay)
-       time.sleep(1)  # one second
-
-   return vars
-
-if __name__ == '__main__':
-   ping("google.co.il")
